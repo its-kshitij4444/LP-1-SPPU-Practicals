98ed74cfe014713c619129a6433fbf2a6957dfa0
its-kshitij4444 <kjagtap2004@gmail.com>
2024-10-25 20:38:58 +0530

    updated code for macropass2

--- 2b macro pass 2/macropass2.py
+++ 2b macro pass 2/macropass2.py
@@ -1,72 +1,73 @@
 import os
-cwd=os.getcwd()
+
+# Define paths to files
 opfile = os.path.join('.', '2b macro pass 2', 'OUTPUT.txt')
 icfile = os.path.join('.', '2a macro pass 1', 'IC.txt')
 mdtfile = os.path.join('.', '2a macro pass 1', 'MDT.txt')
 mntfile = os.path.join('.', '2a macro pass 1', 'MNT.txt')
 
-ic = open(icfile,"r")
-mnt = open(mntfile,"r")
-mdt = open(mntfile,"r")
-i = ic.readlines()
-n = mnt.readlines()
-m = mdt.readlines()
-ic.close()
-mnt.close()
-mdt.close()
+# Read input files
+with open(icfile, "r") as ic, open(mntfile, "r") as mnt, open(mdtfile, "r") as mdt:
+    i = ic.readlines()
+    n = mnt.readlines()
+    m = mdt.readlines()
 
-f = open(opfile,"w")
-for line in i:
-    flag = 0
-    temp = str(line).split()
-    print(temp)
-    for i2 in n:
-        t = str(i2).split()
-        if t[0] == temp[0]:
-            flag = 1
-            mdpt = int(str(t[1]))
-            break
-    if flag == 1:
-        ala = str(temp[1]).split(",")
-        flag += 1
-    if flag > 1:
-        lis = []
-        for i2 in range(mdpt-1,len(m)):
-            st = str(m[i2])[0:len(m[i2])-1]
-            if st == "MEND":
+# Open output file
+with open(opfile, "w") as f:
+    # Iterate over each line in the intermediate code (IC)
+    for line in i:
+        flag = 0
+        temp = str(line).strip().split()
+        
+        # Check if the current line matches any macro name in the MNT
+        for mnt_line in n:
+            t = str(mnt_line).strip().split()
+            if t[0] == temp[0]:
+                flag = 1
+                mdpt = int(t[1])
                 break
-            else:
+        
+        if flag == 1:
+            # Parse arguments from IC line
+            ala = str(temp[1]).split(",") if len(temp) > 1 else []
+            flag += 1
+            
+        if flag > 1:
+            # Collect MDT lines corresponding to the macro definition
+            lis = []
+            for line_idx in range(mdpt, len(m)):
+                st = str(m[line_idx]).strip()
+                if st == "MEND":
+                    break
                 lis.append(st)
-        ala2 = []
-        for item in range(len(lis)):
-            tmp = str(lis[item]).split()
-            if item == 0:
-                ala2 = str(tmp[1]).split(",")
-            if item > 0:
-                f.write(tmp[0]+" ")
-            tmp = str(tmp[1]).split(",")
-            buffer = ""
-            for k in tmp:
-                for ii in range(len(ala2)):
-                    if k == "#"+str(ii):
-                        if len(ala) < len(ala2):
-                            for l in range(len(ala2)):
-                                aflag=0
-                                for ll in range(len(ala2[l])):
-                                    if ala2[l][ll] == '=':
-                                        aflag = 1
-                                if aflag == 1 and ll > l:
-                                    ala2[l] = str(ala2[l]).split("=")[1]
-                                else:
-                                    ala2[l] = ala[l]
-                                ala = ala2
-                        h = ala[ii].split("=")
-
-                        if len(h) == 2:
-                            buffer += (str(h[1])+",")
-                        else:
-                            buffer += (str(h[0])+",")
-            if item > 0:
-                f.write(buffer[0:len(buffer)-1]+"\n")
-    elif flag == 0:
-        f.write(line)
+            
+            # Parse the macro definition arguments from MDT
+            ala2 = []
+            for item_idx, item in enumerate(lis):
+                tmp = str(item).split()
+                if item_idx == 0:
+                    ala2 = str(tmp[1]).split(",")
+                
+                if item_idx > 0:
+                    # Write the macro instruction name
+                    f.write(tmp[0] + " ")
+                
+                # Substitute parameters with actual arguments
+                tmp_args = str(tmp[1]).split(",")
+                buffer = ""
+                
+                for k in tmp_args:
+                    for ii, arg in enumerate(ala2):
+                        if k == f"#{ii}":
+                            # Substitute arguments, handle default values
+                            if len(ala) <= ii:
+                                default_value = ala2[ii].split("=")[1] if "=" in ala2[ii] else ""
+                                buffer += default_value + ","
+                            else:
+                                buffer += ala[ii] + ","
+                
+                if item_idx > 0:
+                    f.write(buffer.rstrip(",") + "\n")
+        elif flag == 0:
+            # Write non-macro lines directly
+            f.write(line)
